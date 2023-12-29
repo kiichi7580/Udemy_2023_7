bbdefda45aee19646498bda584db7a1d8ebc152f
kiichi7580 <kiichi.nakazato0521@gmail.com>
2023-12-30 02:36:22 +0900

    first commit

---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key});
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  void _pickImage() {
+    ImagePicker().
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+        ),
+        TextButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.image),
+          label: Text(
+            'Add Image',
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
